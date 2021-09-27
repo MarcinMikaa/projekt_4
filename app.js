91e593f47d18d628e4b38849ee71df25b2283af1
Marcin <marcin.mika123@gmail.com>
2021-09-27 08:39:46 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const port = process.env.PORT || 3000
+
+const generateTitle = () => {
+    return 'Node js'
+}
+
+
+const app = express()
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+
+    const title = generateTitle()
+
+    res.render('index', {
+        pageTitle: title,
+        pageBody: 'hello node'
+    })
+})
+
+app.get('/kontakt', (req, res) => {
+    res.send('<html> <title>...')
+})
+
+app.listen(port)
